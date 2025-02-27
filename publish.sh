3a8f526c945de4e37be06a2e5dad533c1b6098a3
rindy1123 <genryusinbaou@gmail.com>
2025-02-27 16:16:19 +0900

    fix: publish to storacha
    Notion:

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
+hugo
 cp  _redirects.template public/_redirects
-ipfs add -r public
-ipfs add -Qr --only-hash public | xargs ipfs pin add
-echo "/ipfs/$(ipfs add -Qr --only-hash public)" | xargs ipfs name publish
+ipfs add -r --cid-version 1 public
+ipfs add -Qr --cid-version 1 --only-hash public | xargs ipfs pin add
+echo "/ipfs/$(ipfs add -Qr --cid-version 1 --only-hash public)" | xargs ipfs name publish
+w3 up public/
